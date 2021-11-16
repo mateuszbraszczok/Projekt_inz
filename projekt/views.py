fb25d52736a271d4d37eef5d8a6656da619734ff
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2021-11-16 23:46:51 +0100

    added charts

--- projekt/views.py
+++ projekt/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime, timedelta
 
 from .models import Measurements
+
+from .utils import get_plot
 
 # Create your views here.
 
@@ -9,15 +12,17 @@ def index(request):
     return render(request,"index.html")
 
 def poziom(request):
-    # output = "<h1> poziom pomiary </h1>"
-    latest_measurements_list = Measurements.objects.order_by('-timestamp')[:10]
-    print(latest_measurements_list)
-    # output += ',</br> '.join([ "{}     <b>{:.3f}</b>".format(str(q.timestamp),float(q.poziom))   for q in latest_measurements_list])
-    # return HttpResponse(output)
+    
+
+    time_threshold = datetime.now() - timedelta(hours=5)
+    latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold)
     return render(request,"poziom.html", {'latest_measurements_list': latest_measurements_list})
 
 def natlenienie(request):
-    latest_measurements_list = Measurements.objects.order_by('-timestamp')[:10]
-    output = "<h1> natlenienie pomiary </h1>"
-    output += ',</br> '.join([ "{}     <b>{:.3f}</b>".format(str(q.timestamp),float(q.natlenienie))   for q in latest_measurements_list])
-    return HttpResponse(output)
+    time_threshold = datetime.now() - timedelta(minutes=2)
+    latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold)
+    x = [x.timestamp for x in latest_measurements_list]
+    y = [y.natlenienie for y in latest_measurements_list]
+    chart = get_plot(x, y)
+
+    return render(request,"natlenienie.html", {'latest_measurements_list': latest_measurements_list, 'chart': chart})
