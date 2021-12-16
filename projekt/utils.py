aa6276c7db3541dccfba99fc925df82879ad82a0
Mateusz Braszczok <mateusz.braszczok@gmail.com>
2021-12-16 20:13:21 +0100

    better plot scalling

--- projekt/utils.py
+++ projekt/utils.py
@@ -9,7 +9,6 @@ from matplotlib.dates import DateFormatter
 from pythonFiles.variables import params
 
         
-
 def get_graph():
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
@@ -22,77 +21,142 @@ def get_graph():
     return graph
 
 
-def get_plot(title, x, y, y2 = None, y3=None):
-    plt.switch_backend('AGG')
-    plt.figure(figsize=(12,8))
-    plt.title(title)
+def get_plot(variables, x, y, y2 = None, y3=None):
+    # plt.switch_backend('AGG')
+    # plt.figure(figsize=(12,8))
+    # plt.title("Plot")
 
+    # host = host_subplot(111, axes_class=AA.Axes)
+    # plt.subplots_adjust(right=0.7)
+
+    # if y2 is not None:
+    #     par1 = host.twinx()
+    #     par2 = host.twinx()
+    #     offset = 0
+    #     new_fixed_axis = par1.get_grid_helper().new_fixed_axis
+    #     par1.axis["right"] = new_fixed_axis(loc="right",
+    #                                         axes=par1,
+    #                                         offset=(offset, 0))
+
+    #     par1.axis["right"].toggle(all=True)
+
+    # if y3 is not None:
+    #     offset = 50
+    #     new_fixed_axis = par2.get_grid_helper().new_fixed_axis
+    #     par2.axis["right"] = new_fixed_axis(loc="right",
+    #                                         axes=par2,
+    #                                         offset=(offset, 0))
+    #     par2.axis["right"].toggle(all=True)
+    
+
+    # p1, = host.plot(x, y, label=params[variables[0]])   
+    # minY = min(y) * 0.8
+    # maxY = max(y) * 1.2
+    # host.set_ylim(minY, maxY)
+
+    # if y2 is not None:
+    #     p2, = par1.plot(x, y2, label=params[variables[1]])
+    #     minY2 = min(y2) * 0.8
+    #     maxY2 = max(y2) * 1.4
+    #     par1.set_ylim(minY2, maxY2)
+    # if y3 is not None:
+    #     p3, = par2.plot(x, y3, label=params[variables[2]])
+    #     minY3 = min(y3) * 0.7
+    #     maxY3 = max(y3) * 1.4
+    #     par2.set_ylim(minY3, maxY3)
+
+
+    # #host.set_xlabel("Time")
+    # host.legend()
+    # host.set_ylabel(params[variables[0]])
+
+    # host.axis["left"].label.set_color(p1.get_color())
+    # host.grid()
+    # host.axis('off')
+    # if y2 is not None:
+    #     par1.axis["right"].label.set_color(p2.get_color())
+    #     par1.set_ylabel(params[variables[1]])
+
+    # if y3 is not None:
+    #     par2.axis["right"].label.set_color(p3.get_color())
+    #     par2.set_ylabel(params[variables[2]])
+    
+    # plt.tight_layout(pad=2)
+    # plt.plot(x, y, label=params[variables[0]])
+    # plt.xticks([], [])
+    # #plt.yticks([], [])
+    # #plt.yticks([])
+    # # plt.grid()
+    # # plt.xticks(rotation=45)
+    # # plt.gcf().autofmt_xdate()
+    # # ax = plt.gca()
+    # # myFmt = DateFormatter('%H:%M:%S')
+    # # ax.xaxis.set_major_formatter(myFmt)
+    # # ax.set_ylim([0, 10])
+
+
+
+    time = np.arange(10)
+    temp = np.random.random(10)*30
+    Swdown = np.random.random(10)*100-10
+    Rn = np.random.random(10)*100-10
+
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(10,8))
+    plt.title("Plot")
 
     host = host_subplot(111, axes_class=AA.Axes)
-    plt.subplots_adjust(right=0.7)
 
+    fig = plt.figure()
+    fig.set_size_inches(12, 8)
+    ax = fig.add_subplot(111)
 
+    label = params[variables[0]]
+    label = label[0]['label']
+    lns1 = ax.plot(x, y, '-bo', markersize=6, label=label )
+    
+    lns = lns1
+    
+    ax.set_ylabel(label)
+    ax.yaxis.label.set_color('blue')
     if y2 is not None:
-        par1 = host.twinx()
-        par2 = host.twinx()
-        offset = 0
-        new_fixed_axis = par1.get_grid_helper().new_fixed_axis
-        par1.axis["right"] = new_fixed_axis(loc="right",
-                                            axes=par1,
-                                            offset=(offset, 0))
-
-        par1.axis["right"].toggle(all=True)
+        ax2 = ax.twinx()
+        label = params[variables[1]]
+        label = label[0]['label']
+        lns2 = ax2.plot(x, y2, '-ro', markersize=4, label=label)
+        lns += lns2
+        ax2.set_ylabel(label)
+        ax2.yaxis.label.set_color('red')
 
     if y3 is not None:
-        offset = 50
-        new_fixed_axis = par2.get_grid_helper().new_fixed_axis
-        par2.axis["right"] = new_fixed_axis(loc="right",
-                                            axes=par2,
-                                            offset=(offset, 0))
-        par2.axis["right"].toggle(all=True)
+        ax3 = ax.twinx()
+        label = params[variables[2]]
+        label = label[0]['label']
+        ax3.spines['right'].set_position(("axes", 1.12))
+
+        lns3 = ax3.plot(x, y3, '-go', markersize=3, label=label)
+        lns += lns3
+        ax3.set_ylabel(label)
+        ax3.yaxis.label.set_color('green')
+
+    # added these three lines
+
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs, loc=0)
+
+    ax.grid()
+    ax.set_xlabel("Time")
     
 
-    p1, = host.plot(x, y, label="Temperature")
-    minY = min(y) * 0.8
-    maxY = max(y) * 1.2
-    host.set_ylim(minY, maxY)
-    if y2 is not None:
-        p2, = par1.plot(x, y2, label="Redox")
-        minY2 = min(y2) * 0.8
-        maxY2 = max(y2) * 1.4
-        par1.set_ylim(minY2, maxY2)
-    if y3 is not None:
-        p3, = par2.plot(x, y3, label="Turbidity")
-        minY3 = min(y3) * 0.7
-        maxY3 = max(y3) * 1.4
-        par2.set_ylim(minY3, maxY3)
+    # ax2.set_ylim(0, 35)
+    # ax.set_ylim(-20,100)
+    plt.subplots_adjust(left=0.1, right=0.8, top=0.9, bottom=0.05)
 
-
-    #host.set_xlabel("Time")
-    host.legend()
-    host.set_ylabel("Temperature")
-
-    host.axis["left"].label.set_color(p1.get_color())
-    host.grid()
-    if y2 is not None:
-        par1.axis["right"].label.set_color(p2.get_color())
-        par1.set_ylabel("Redox")
-
-    if y3 is not None:
-        par2.axis["right"].label.set_color(p3.get_color())
-        par2.set_ylabel("Turbidity")
-    
-    plt.tight_layout(pad=2)
-    # plt.xticks([], [])
-    # plt.yticks([], [])
-    #plt.yticks([])
-    # plt.grid()
-    # plt.xticks(rotation=45)
-    # plt.gcf().autofmt_xdate()
-    # ax = plt.gca()
-    # myFmt = DateFormatter('%H:%M:%S')
-    # ax.xaxis.set_major_formatter(myFmt)
-    # ax.set_ylim([0, 10])
+    plt.xticks(rotation=45)
+    plt.gcf().autofmt_xdate()
+    ax = plt.gca()
+    myFmt = DateFormatter('%H:%M:%S')
+    ax.xaxis.set_major_formatter(myFmt)
     graph = get_graph()
 
     return graph
--- projekt/views.py
+++ projekt/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from datetime import datetime, timedelta, date
 from dateutil import parser
 from django.db.models import Avg, Max, Min
+from django.contrib.auth.decorators import login_required
 from .models import Measurements
 
 from django.utils import timezone
@@ -15,6 +16,7 @@ from pythonFiles.variables import tags
 def index(request):
     return render(request,"projekt/index.html")
 
+@login_required(login_url='/login')
 def poziom(request):
     time_threshold = datetime.now() - timedelta(hours=1)
     latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold)
@@ -29,35 +31,40 @@ def natlenienie(request):
 
     return render(request,"projekt/natlenienie.html", {'latest_measurements_list': latest_measurements_list, 'chart': chart})
 
-    
+@login_required(login_url='/login')    
 def dane1(request, var1, minutes):
     # letter_list = var1.split(",")
     # print(letter_list)
     possibleVariables = tags.copy()
 
     if var1 in tags:
-        possibleVariables.remove(var1)
+        #possibleVariables.remove(var1)
         time_threshold = timezone.now() - timedelta(minutes=minutes)
         latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold).order_by("-id")
         infoList = latest_measurements_list.aggregate(Avg(var1), Max(var1), Min(var1))
         toExecute = "[y."+ var1 +" for y in latest_measurements_list]"
         x = [x.timestamp for x in latest_measurements_list]
         y = eval(toExecute)
-        resultList = zip(x,y)
-        chart = get_plot(var1, x, y)
+        resultList = zip(x, y)
+        variables = [var1]
+        chart = get_plot(variables, x, y)
         
-        returnDict = {'resultList': resultList, 'chart': chart, 'type': var1, 'possibleVariables': possibleVariables, 'minute': minutes, 'infoList': infoList}
+        
+        returnDict = {'resultList': resultList, 'chart': chart, 'type': var1, 'possibleVariables': possibleVariables, 'minute': minutes, 'infoList': infoList,
+            'noOfVariables': 1,
+            'variables': variables}
         return render(request, "projekt/data.html", returnDict )
     else:
         return HttpResponseNotFound("Page not found") 
 
+@login_required(login_url='/login')
 def dane2(request, var1, var2, minutes):
     # letter_list = var1.split(",")
     # print(letter_list)
     possibleVariables = tags.copy()
 
     if var1 in tags:
-        possibleVariables.remove(var1)
+        #possibleVariables.remove(var1)
         time_threshold = timezone.now() - timedelta(minutes=minutes)
         latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold).order_by("-id")
         infoList = latest_measurements_list.aggregate(Avg(var1), Max(var1), Min(var1))
@@ -66,21 +73,31 @@ def dane2(request, var1, var2, minutes):
         x = [x.timestamp for x in latest_measurements_list]
         y1 = eval(toExecute1)
         y2 = eval(toExecute2)
-        resultList = zip(x,y1)
-        chart = get_plot(var1, x, y1, y2)
-        
-        returnDict = {'resultList': resultList, 'chart': chart, 'type': var1, 'possibleVariables': possibleVariables, 'minute': minutes, 'infoList': infoList}
+        resultList = zip(x, y1, y2)
+
+        variables = [var1, var2]
+        chart = get_plot(variables, x, y1, y2)
+            
+        returnDict = {'resultList': resultList, 
+            'chart': chart, 
+            'type': var1, 
+            'possibleVariables': possibleVariables, 
+            'minute': minutes, 
+            'infoList': infoList,
+            'noOfVariables': 2,
+            'variables': variables }
         return render(request, "projekt/data.html", returnDict )
     else:
         return HttpResponseNotFound("Page not found") 
 
+@login_required(login_url='/login')
 def dane3(request, var1, var2, var3, minutes):
     # letter_list = var1.split(",")
     # print(letter_list)
     possibleVariables = tags.copy()
-
+    
     if var1 in tags:
-        possibleVariables.remove(var1)
+        #possibleVariables.remove(var1)
         time_threshold = timezone.now() - timedelta(minutes=minutes)
         latest_measurements_list = Measurements.objects.filter(timestamp__gt=time_threshold).order_by("-id")
         infoList = latest_measurements_list.aggregate(Avg(var1), Max(var1), Min(var1))
@@ -91,10 +108,19 @@ def dane3(request, var1, var2, var3, minutes):
         y1 = eval(toExecute1)
         y2 = eval(toExecute2)
         y3 = eval(toExecute3)
-        resultList = zip(x,y1)
-        chart = get_plot(var1, x, y1, y2, y3)
+        resultList = zip(x, y1, y2, y3)
         
-        returnDict = {'resultList': resultList, 'chart': chart, 'type': var1, 'possibleVariables': possibleVariables, 'minute': minutes, 'infoList': infoList}
+
+        variables = [var1, var2, var3]
+        chart = get_plot(variables, x, y1, y2, y3)
+        returnDict = {'resultList': resultList, 
+            'chart': chart, 
+            'type': var1, 
+            'possibleVariables': possibleVariables, 
+            'minute': minutes, 
+            'infoList': infoList,
+            'noOfVariables': 3,
+            'variables': variables}
         return render(request, "projekt/data.html", returnDict )
     else:
         return HttpResponseNotFound("Page not found") 
@@ -106,6 +132,7 @@ def schemat(request):
     temperaturePercent["temperaturePercent"] = ((40.0 - latest_measurements_list.Temperature)/ 25.0) *100.0
     return render(request,"projekt/schemat.html", {'level': level, 'latest_measurements_list': latest_measurements_list, "temperaturePercent":temperaturePercent})
 
+@login_required(login_url='/login')
 def viewChange(request):
     if request.method == 'POST':
         POSTValues = request.POST
@@ -113,7 +140,6 @@ def viewChange(request):
         for tag in tags:
             if tag in request.POST:
                 tagList.append(str(tag))
-                print(tag)
 
         if len(tagList) == 1:
             returnHtml = "/projekt/data/{}/minutes/{}".format(tagList[0], POSTValues['minutes'])
@@ -126,6 +152,7 @@ def viewChange(request):
         return HttpResponseNotFound("Page not found") 
 
 
+@login_required(login_url='/login')
 def psg(request, date):
     query="date.values_list("
     for tag in tags:
@@ -144,6 +171,7 @@ def psg(request, date):
     
     return response
 
+@login_required(login_url='/login')
 def history(request, year=None, month=None, day=None):
     if(year ==None or month == None or day == None):
         latest_measurements_list = Measurements.objects.filter(timestamp__gte = date.today())
@@ -184,6 +212,7 @@ def history(request, year=None, month=None, day=None):
     # return render(request,"history.html", {'returnDict': returnDict})
     return response
 
+@login_required(login_url='/login')
 def dateChange(request):
     if request.method == 'POST':
         POSTValues = request.POST
